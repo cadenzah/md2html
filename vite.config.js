f2c7030cffa157b6cacd7a27aad197b9c136c541
cadenzah <cadenzah93@gmail.com>
2023-12-29 16:51:50 +0900

    scaffolding
    - vite 설정
    - eslint, prettier
    - poc에 필요한 최소 파일들

---
+++ vite.config.js
@@ -0,0 +1,62 @@
+import path from 'path';
+import { defineConfig } from 'vite';
+import reactPlugin from '@vitejs/plugin-react';
+import eslintPlugin from "@nabla/vite-plugin-eslint";
+import htmlPlugin from 'vite-plugin-html-config';
+
+// import { configureEnv } from './src/utils/server/configureEnv';
+
+const ENV_DIR = process.cwd();
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode, ssrBuild }) => {
+  const env = configureEnv(ENV_DIR, '_', { mode, ssrBuild });
+
+  return {
+    resolve: {
+      alias: {
+          '~': path.resolve(__dirname),
+          '@': path.resolve(__dirname, 'src')
+      }
+    },
+    plugins: [
+      eslintPlugin(),
+      reactPlugin(),
+      htmlPlugin({
+        title: env._PAGE_TITLE,
+        metas: [
+          {
+            name: 'title',
+            content: env._PAGE_TITLE,
+          },
+          {
+            name: 'description',
+            content: env._PAGE_DESCRIPTION,
+          }
+        //   {
+        //     property: 'og:url',
+        //     content: `${env._PAGE_URL}/`,
+        //   },
+        //   {
+        //     property: 'og:title',
+        //     content: env._PAGE_TITLE,
+        //   },
+        //   {
+        //     property: 'og:description',
+        //     content: env._PAGE_DESCRIPTION,
+        //   },
+        //   {
+        //     property: 'og:image',
+        //     content: `${env._PAGE_URL}/preview.jpg`
+        //   }
+        ]
+      })
+    ],
+    define: {
+      'APP_ENV': env
+    },
+    base: env.BASE_URL,
+    publicDir: 'src/public',
+    envPrefix: '_'
+  };
+});
